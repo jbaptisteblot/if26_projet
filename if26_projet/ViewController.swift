c4abe3d71cacc21e7038240418d5a80ccd13ec2f
Jean-baptiste Blot <jean-baptisteblot@Jean-baptistes-Mac.local>
2017-11-30 06:43:35 -0800

    Initial Commit

---
+++ if26_projet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  if26_projet
+//
+//  Created by Jean-baptiste Blot on 30/11/2017.
+//  Copyright © 2017 Jean-baptiste Blot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
