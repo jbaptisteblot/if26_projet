71f3b0cac8f91e52bc17b3e99c9374a2deae1c8a
Peirun <peirun.yu@gmail.com>
2017-12-19 12:39:54 +0100

    Rafraîchissement automatique de la liste des trajets mémorisés.

--- if26_projet/TrajetsTableViewController.swift
+++ if26_projet/TrajetsTableViewController.swift
@@ -19,12 +19,17 @@ class TrajetsTableViewController: UITableViewController {
     var trajets:[Trajet] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        reloadData()
+        //reloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reloadData()
     }
     
     func reloadData() {
