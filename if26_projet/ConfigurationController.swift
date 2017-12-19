083c1524bf35f4306500f99e387d4a7ac5e15a8c
Peirun <peirun.yu@gmail.com>
2017-12-19 15:54:22 +0100

    Ajustements interface et motifs mineures je sais plus trop où, désolé. Oui ce message de commit est utile.

--- if26_projet/ConfigurationController.swift
+++ if26_projet/ConfigurationController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ConfigurationController: UIViewController {
     @IBOutlet weak var SNCFApikeyInput: UITextField!
+    @IBOutlet weak var APILabel: UILabel!
+    @IBOutlet weak var RAZLabel1: UILabel!
+    @IBOutlet weak var RAZLabel2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        APILabel.text = "Afin de pouvoir accéder aux données en ligne de la SNCF, il est nécessaire d'utiliser une clé d'API développeur, à renseigner ici :"
+        RAZLabel1.text = "Cette fonction permet de créer ou recréer les tables de la base de données, lors de la première utilisation de l'application sur un nouvel appareil ou en cas de problème avec les tables."
+        RAZLabel2.text = "Attention : Ceci supprimera toutes les données enregistrées."
+        APILabel.sizeToFit()
+        RAZLabel1.sizeToFit()
+        RAZLabel2.sizeToFit()
+        
         SNCFApikeyInput.insertText(APIKEY.SNCF)
         // Do any additional setup after loading the view.
     }
